224374271ac581dee1bfd2ec650ac3f3ebeb0af4
IntelSDM <GitBackup@shibemaster.me>
2023-07-07 21:31:22 +0100

    Text Drawing

--- Overlay/Graphics/Drawing.cpp
+++ Overlay/Graphics/Drawing.cpp
@@ -1,22 +1,45 @@
 #include "pch.h"
 #include "Graphics.h"
 #include "Drawing.h"
-using namespace Windows::UI;
-using namespace Microsoft::Graphics::Canvas::UI::Xaml;
-using namespace Microsoft::Graphics::Canvas;
-using namespace Windows::Foundation;
-using namespace Windows::UI::Core;
-using namespace Windows::System;
-using namespace Windows::UI::ViewManagement;
-using namespace Windows::ApplicationModel::Core;
-using namespace Microsoft::Graphics::Canvas::UI::Xaml;
-using namespace Microsoft::Graphics::Canvas;
-using namespace Microsoft::Graphics::Canvas::Text;
-using namespace Windows::UI::Text;
+#include "Font.h"
 
 
 
 void Text(int x, int y,std::wstring text,std::string font, int fontsize,Color colour,FontAlignment alignment)
 {
+	Platform::String^ platstring = ref new Platform::String(text.data());
+	CanvasTextLayout^ layout = ref new CanvasTextLayout(SwapChain,platstring, GetFont(font),4096.0f,4096.0f);
+	layout->SetFontSize(0,text.length(),fontsize);
+	Point point(x, y);
+	float modifier = layout->DefaultFontSize / 4.0f; // metrics isn't ever correct
 
+    switch (alignment)
+    {
+    case FontAlignment::Centre:
+        x -= ((layout->LayoutBounds.Width + modifier) / 2);
+        break;
+    case FontAlignment::Right:
+        x += ((layout->LayoutBounds.Width + modifier));
+        break;
+    case FontAlignment::Left:
+        x -= ((layout->LayoutBounds.Width + modifier));
+        break;
+    case FontAlignment::None:
+        break;
+    case FontAlignment::CentreCentre:
+        x -= ((layout->LayoutBounds.Width + modifier) / 2);
+        y -= ((layout->LayoutBounds.Height + modifier) / 2);
+        break;
+    case FontAlignment::CentreLeft:
+        x -= ((layout->LayoutBounds.Width + modifier));
+        y += ((layout->LayoutBounds.Height + modifier) / 2);
+        break;
+    case FontAlignment::CentreRight:
+        x += ((layout->LayoutBounds.Width + modifier));
+        y -= ((layout->LayoutBounds.Height + modifier) / 2);
+        break;
+    }
+    point.X = x;
+    point.Y = y;
+	SwapChain->DrawTextLayout(layout, point, colour);
 }
